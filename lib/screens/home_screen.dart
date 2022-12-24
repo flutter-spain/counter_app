0e2070ca6861d241d4b860733e06b8ede97e2dce
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2022-12-24 04:16:24 +0100

    51

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
   
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-      return const Center(
-            child: Text('Home Screen')
-            );
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Home Screen'),
+          elevation: 10,
+        ),
+        backgroundColor: Colors.white,
+        body :  Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                // ignore: prefer_const_literals_to_create_immutables
+                children: <Widget>[
+                  const Text(
+                    'Números de Clicks:',
+                  ),
+                  const Text(
+                    '0',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Add your onPressed code here!
+            
+          },
+          tooltip: 'Incrementar',
+          child: const Icon(Icons.add),
+        ),
+      );
   }
-
 }
